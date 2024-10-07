5e7b64f561459fb4670f03b8c4f24e3c07147880
unknown <ishabenpiyushkuma@loyalistcollege.com>
2024-10-07 12:15:38 -0400

    Added get function

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 // PhoneCompany struct to define a phone company
 type PhoneCompany struct {
 	ID          int    `json:"id"`
@@ -7,4 +13,16 @@ type PhoneCompany struct {
 	Country     string `json:"country"`
 	FoundedYear int    `json:"founded_year"`
 	Description string `json:"description"`
+}
+
+var companies []PhoneCompany
+var idCounter int = 1
+
+// Helper function to get the company ID from the URL
+func getIDFromURL(path string) (int, error) {
+	parts := strings.Split(path, "/")
+	if len(parts) != 3 {
+		return 0, fmt.Errorf("invalid URL")
+	}
+	return strconv.Atoi(parts[2])
 }
