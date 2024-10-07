c85beeeb265b5966d44eefcfeac58f4adfbc54d0
unknown <ishabenpiyushkuma@loyalistcollege.com>
2024-10-07 12:20:58 -0400

    Added View function

--- main.go
+++ main.go
@@ -50,3 +50,9 @@ func createCompany(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(newCompany)
 }
+
+// Get all phone companies (GET /companies)
+func getCompanies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(companies)
+}
